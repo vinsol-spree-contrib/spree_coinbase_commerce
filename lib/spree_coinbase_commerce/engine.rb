16f1ed819e4e052731e7727e414efdc10af1c06a
Siddharth Sharma <siddharth10104737@gmail.com>
2018-10-24 19:10:51 +0530

    minor fix

--- lib/spree_coinbase_commerce/engine.rb
+++ lib/spree_coinbase_commerce/engine.rb
@@ -12,7 +12,6 @@ module SpreeCoinbaseCommerce
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
-        Rails.application.config.spree.payment_methods << Spree::PaymentMethod::Coinbase
       end
     end
 
